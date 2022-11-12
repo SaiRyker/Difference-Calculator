d2fc3555f17a1a8870741b6efdf4a7360c729339
SaiRyker <daniichernyshov0@gmail.com>
2022-11-12 16:17:33 +0300

    step 2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,5 +7,8 @@ const program = new Command();
 program
     .description("Compares two configuration files and shows a difference.")
     .version('0.8.0')
+    .arguments("<filepath1>")
+    .argument("<filepath2>")
+    .option('-f, --format [type]', 'output format')
 
 program.parse(process.argv);
